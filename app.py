edfe1036fe12e0d95c56d5d8dc8ec13c3a2bfa6a
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-03-18 11:06:03 +0000

    created products list, render with HTML and improve the front page of the website.

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+PRODUCTS = [
+  {
+    'id': 1,
+    'item': 'Sofa',
+    'category': 'Living Room Furniture',
+    'color': 'Gray',
+    'price': '800'
+  },
+  {
+    'id': 2,
+    'item': 'Dining Table',
+    'category': 'Dining Room Furniture',
+    'color': 'Brown',
+    'price': '1200'
+  },
+  {
+    'id': 3,
+    'item': 'Bed',
+    'category': 'Bedroom Furniture',
+    'price': '1500'
+  },
+  {
+    'id': 4,
+    'item': 'Armchair',
+    'category': 'Living Room Furniture',
+    'color': 'Blue',
+    'price': '500'
+  },
+]
+
 
 @app.route("/")
-def index():
-  return render_template('home.html')
+def hello_modern():
+  return render_template('home.html', products=PRODUCTS, company_name="Modern")
+
+
+@app.route("/products")
+def list_products():
+  return jsonify(PRODUCTS)
 
 
 if __name__ == "__main__":
